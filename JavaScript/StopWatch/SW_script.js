845bb36ab94dc8f00ea61636d409548654fe58d7
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2022-08-13 00:29:55 +0900

    Add files via upload

--- JavaScript/StopWatch/SW_script.js
+++ JavaScript/StopWatch/SW_script.js
@@ -22,7 +22,7 @@ const recordTimes = document.getElementById("r_times");
 
 let step;
 let intevalId;
-
+let lapNum;
 buttonStart.onclick = function(){
     intevalId = setInterval(operateTimer ,10);
 }
@@ -32,19 +32,17 @@ buttonbStop.onclick=function(){
 }
 //record를 
 buttonRecord.onclick=function(){
-// 기존 레코드
+// last lap
     r_minutes = minutes; r_seconds = seconds ;r_tenMillis = tenMillis;
     recordMin.textContent = r_minutes> 9 ? r_minutes : "0"+ r_minutes;
     recordSec.textContent = r_seconds> 9 ? r_seconds : "0"+ r_seconds;
     recordTen.textContent = r_tenMillis> 9 ? r_tenMillis : "0"+ r_tenMillis;
 // 레코드 리스트 
-    record_list.push(recordMin.textContent +' : '+ recordSec.textContent +' :'+ recordTen.textContent);
+    lapNum = record_list.length+1
+    record_list.push("#"+lapNum+'. '+recordMin.textContent +' : '+ recordSec.textContent +' : '+ recordTen.textContent);
     console.log(record_list.length)
-// 레코드 리스트 요소들을 전부 출력
-    for(step = 0; step < record_list.length; step++){
-        recordTimes.textContent = record_list[step];
-        console.log(record_list[step]);
-        }
+// 레코드 리스트 요소들을 전부 출력 all lap
+    recordTimes.innerHTML = record_list.join('<br>');
     }
 
 
@@ -54,6 +52,7 @@ buttonReset.onclick=function(){
     r_minutes = 0; r_seconds = 0; minutes = 0;
     appendTen.textContent = "00"; appendSec.textContent = "00"; appendMin.textContent = "00";
     recordMin.textContent ="00";recordSec.textContent="00";recordTen.textContent="00";
+    record_list = [];recordTimes.innerHTML = "#1. 00 : 00 : 00";
 }
 
 
