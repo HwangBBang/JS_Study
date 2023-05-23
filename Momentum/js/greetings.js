882bdeebc603ec996bcca1063653b1af7ce650c3
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2023-05-24 00:29:07 +0900

    Add files via upload

---
+++ Momentum/js/greetings.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const userName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreetings(userName);
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username} !`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedName = localStorage.getItem(USERNAME_KEY);
+
+if (savedName === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  //greeting
+  paintGreetings(savedName);
+}
